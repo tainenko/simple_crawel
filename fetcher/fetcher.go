ecbc36c79d84fa2d15ff4909f44d94ca2e992ef9
ko tainen <TonyK@hyvesolutions.com>
2020-02-23 21:17:18 +0800

    Add header in request

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -3,17 +3,19 @@ package fetcher
 import (
 	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
-	"golang.org/x/net/html/charset"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func Fetcher(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+func Fetch(url string) ([]byte, error) {
+	request,_ := http.NewRequest(http.MethodGet, url, nil)
+	request.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
+	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
 		return nil, err
 	}
