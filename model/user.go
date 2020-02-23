6cff3e4f1079f34e9800c632d46c439a72f075b8
ko tainen <TonyK@hyvesolutions.com>
2020-02-23 21:27:21 +0800

    init user model

---
+++ model/user.go
@@ -0,0 +1,17 @@
+package model
+
+type User struct{
+	Name string
+	Marriage string
+	Age string
+	Gender string
+	Height string
+	Weight string
+	Income string
+	Education string
+	Occupation string
+	Hukou string
+	Xingzuo string
+	House string
+	Car string
+}
