c9f432b28e1cd0e41f24379a6b2c65604215b141
ko tainen <TonyK@hyvesolutions.com>
2020-02-23 20:00:04 +0800

    add citylistparser.go

---
+++ parser/citylistparser.go
@@ -0,0 +1,22 @@
+package parser
+
+import (
+	"github.com/gocrawl/engine"
+	"regexp"
+)
+
+const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)" [^>]*>([^<]+)</a>`
+
+func ParseCityList(content []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityListRe)
+	all := re.FindAllSubmatch(content, -1)
+	result := engine.ParseResult{}
+	for _, c := range all {
+		result.Items = append(result.Items, string(c[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        string(c[1]),
+			ParserFunc: engine.NilParser,
+		})
+	}
+	return result
+}
