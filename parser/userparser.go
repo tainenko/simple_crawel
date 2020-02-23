eb21025774ff57acb52d74db5ee23a0e696535ea
ko tainen <TonyK@hyvesolutions.com>
2020-02-23 21:39:44 +0800

    add parseJson function

--- parser/userparser.go
+++ parser/userparser.go
@@ -3,6 +3,9 @@ package parser
 import (
 	"fmt"
 	"github.com/gocrawl/engine"
+	"github.com/gocrawl/model"
+	"github.com/bitly/go-simplejson"
+	"log"
 	"regexp"
 )
 
@@ -12,8 +15,41 @@ func ParseUserInfo(contents []byte) engine.ParseResult{
 	match := re.FindSubmatch(contents)
 	if len(match)>=2{
 		json:=match[1]
-		fmt.Printf("json: %s\n",json)
+		profile:= parseJson(json)
+		fmt.Printf("json: %s\n",profile)
 	}
 	return engine.ParseResult{}
 
+}
+
+func parseJson(json []byte) model.User{
+	res,err:=simplejson.NewJson(json)
+	if err!=nil{
+		log.Print("Failed to parse json.")
+	}
+	infos, err := res.Get("objectInfo").Get("basicInfo").Array()
+	var profile model.User
+	for k,v :=range(infos){
+		if e, ok := v.(string); ok {
+			switch k {
+			case 0:
+				profile.Marriage = e
+			case 1:
+				profile.Age = e
+			case 2:
+				profile.Xingzuo = e
+			case 3:
+				profile.Height = e
+			case 4:
+				profile.Weight = e
+			case 6:
+				profile.Income = e
+			case 7:
+				profile.Occupation = e
+			case 8:
+				profile.Education = e
+			}
+		}
+	}
+	return profile
 }
